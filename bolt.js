c2aafb345393a31f49d5f3ed6a228f4d0cef703b
nvk681 <nvk.681@gmail.com>
2018-02-28 19:52:46 +0530

    adding onclick

--- bolt.js
+++ bolt.js
@@ -2,7 +2,10 @@ $('head').append('<meta name="viewport" content="width=device-width, initial-sca
 $('head').append('<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">');
 //$('body').append('<div class="w3-container"><h2>Circular Buttons</h2><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>');
 //document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
-document.body.innerHTML += '<div class="w3-container"><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>';
+document.body.innerHTML += '<div class="w3-container"><button onclick="botpop()" class="w3-button w3-xlarge w3-circle w3-black">+</button></div>';
 
+function botpop() {
+    console.log("bot is init");
+}
 
 console.log('working');
