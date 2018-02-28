f77c45243589e9c691cc9e074823b9132734bf5a
nvk681 <nvk.681@gmail.com>
2018-02-28 17:18:38 +0530

    made changes

--- bolt.js
+++ bolt.js
@@ -1,2 +1,3 @@
-document.body.innerHTML += '<link rel="stylesheet" type="text/css" href="https://gitlab.com/nvk777/bot-bolt/blob/master/bolt.css">';
-document.body.innerHTML += "<i class='fa fa-plus my-float'></i>";
+document.body.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
+document.body.innerHTML += "<a href='#' class='float'><i class='fa fa-plus my-float'></i>";
