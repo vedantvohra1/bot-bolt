44d57ca3a47daea63edacb181a24233e9fd78ebb
nvk681 <nvk.681@gmail.com>
2018-02-28 18:48:29 +0530

    f

--- bolt.js
+++ bolt.js
@@ -1,3 +1,5 @@
-document.body.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
-document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
-document.body.innerHTML += "<a href='#' class='float'><i class='fa fa-plus my-float'></i>";
+//document.body.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+//document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
+$('head').append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">');
+document.body.innerHTML += '<div class="w3-container"><h2>Circular Buttons</h2><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>';
