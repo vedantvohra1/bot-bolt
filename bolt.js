aea98bc8786b949152a37840a136490160d35519
nvk681 <nvk.681@gmail.com>
2018-02-28 17:12:01 +0530

    nothing

--- bolt.js
+++ bolt.js
@@ -1,2 +1,2 @@
+document.body.innerHTML += '<link rel="stylesheet" type="text/css" href="https://gitlab.com/nvk777/bot-bolt/blob/master/bolt.css">';
 document.body.innerHTML += "<i class='fa fa-plus my-float'></i>";
-document.body.innerHTML += '<link rel="stylesheet" type="text/css" href="/css/versions4.css">'
