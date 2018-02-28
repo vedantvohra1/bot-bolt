4d1c65ef2134c107e84dae560bd20fab257c2f2d
nvk681 <nvk.681@gmail.com>
2018-02-28 19:29:47 +0530

    ads

--- bolt.js
+++ bolt.js
@@ -1,8 +1,8 @@
-//document.body.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
 $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
-$('body').append('<div class="w3-container"><h2>Circular Buttons</h2><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>');
-//document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
 $('head').append('<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">');
+//$('body').append('<div class="w3-container"><h2>Circular Buttons</h2><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>');
+//document.body.innerHTML += '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">';
+document.body.innerHTML += '<div class="w3-container"><button class="w3-button w3-xlarge w3-circle w3-black">+</button></div>';
 
 
 console.log('working');
