49df22b28c7eada10e2b887ad219f17c07833029
nvk681 <nvk.681@gmail.com>
2018-03-03 13:01:01 +0530

    a

--- bolt.js
+++ bolt.js
@@ -12,7 +12,7 @@ document.body.innerHTML += '<div class="w3-container"><button onclick="botpop()"
 //$('body').append('<div class="container"><h2>Modal Example</h2><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button><div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
 
 var recognition;
-var output_class = "#message"
+var output_class = "#message";
 var flags = {}
 var base_url = getBaseURL()
 
@@ -85,7 +85,7 @@ function botpop() {
     console.log("bot is init");
     $('body').append('<div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><div id="messages"></div></div><div class="modal-body"></div><div id="reqblock" class="modal-footer"></div></div></div></div></div>');
     $("#messages").empty();
-    botsay("Hey! How can I help?")
+    botsay("Hey, I am darwin!<br/> How can I help?")
     $("#reqblock").empty();
     $(".modal-body").empty()
     $('#reqblock').append('<input type="text" id="message">');
