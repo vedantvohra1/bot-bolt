0ad186542290fbb2b54c67370cae5d42e0d5a17e
vedantvohra1 <vedantvohra1@github.com>
2018-03-06 20:39:08 +0530

    commented a line

--- bolt.js
+++ bolt.js
@@ -128,7 +128,7 @@ function doit_onkeypress(event) {
 
 function botpop() {
     voiceInit();
-    localStorage.clear();
+    // localStorage.clear();
     console.log("bot is init");
     $('body').append('<div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><div id="messages"></div></div><div class="modal-body"></div><div id="reqblock" class="modal-footer"></div></div></div></div></div>');
     $("#messages").empty();
