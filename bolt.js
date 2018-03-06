cf05f99bfcdf67831a0115e2fc1ced261cb89343
nvk681 <nvk.681@gmail.com>
2018-03-06 11:45:38 +0530

    modified code to execute init

--- bolt.js
+++ bolt.js
@@ -11,29 +11,53 @@ document.body.innerHTML += '<div class="w3-container"><button onclick="botpop()"
 console.log(window.location.href);
 //$('body').append('<div class="container"><h2>Modal Example</h2><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button><div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>');
 
+
+console.log("dummy")
+
+var imgs = {
+    float_icon: "https://cdn3.iconfinder.com/data/icons/customer-support-7/32/40_robot_bot_customer_help_support_automatic_reply-512.png",
+    reply_bot: "https://cdn3.iconfinder.com/data/icons/customer-support-7/32/40_robot_bot_customer_help_support_automatic_reply-512.png",
+}
+
 var recognition;
 var output_class = "#message";
 var flags = {};
 var base_url = getBaseURL();
 
 function getBaseURL() {
-    return 'http://tina.qa.darwinbox.io';
+    return window.location.origin;
 }
-console.log("dummy")
 
-var cdn = "https\://cdn.rawgit.com/vedantvohra1/bot-bolt/4cf46cd8/init/";
+var scripts = "/js/darwin/init_scripts/";
 
-var queryString = window.location.search;
-console.log("query string is :" + queryString);
-if (queryString != "") {
-    var init = localStorage.getItem("init");
-    console.log("init value" + init);
-    if (init) {
-        $('head').append('<script src="' + cdn + init + '.js"></script>');
-        var entities = JSON.parse(localStorage.getItem("entities"));
-        init(entities);
+var setUp = function() {
+    $('head').append('<link rel="stylesheet" type="text/css" href="/css/darwin/bot.css">');
+    document.body.innerHTML += '<span class="w3-container"><img src="' + imgs.float_icon + '" onclick="botpop()" data-toggle="modal" data-target="#myModal" class="w3-button w3-xlarge w3-circle w3-black" width="42px"></span>';
+}
+
+var executeInit = function() {
+    // opened in iframe
+    if (/[?&]bot=/.test(location.search)) {
+        //execute init scripts
+        $('body').append('<script src="' + scripts + localStorage.getItem('init') + '.js"></script>');
+    } else {
+        setUp();
     }
 }
+executeInit();
+
+
+// var queryString = window.location.search;
+// console.log("query string is :" + queryString);
+// if (queryString != "") {
+//     var init = localStorage.getItem("init");
+//     console.log("init value" + init);
+//     if (init) {
+//         $('head').append('<script src="' + cdn + init + '.js"></script>');
+//         var entities = JSON.parse(localStorage.getItem("entities"));
+//         init(entities);
+//     }
+// }
 
 
 
@@ -53,52 +77,26 @@ function message() {
     var time = h + ':' + m;
     console.log(text);
     if (text != "") {
-        $('#messages').append('<div class="container darker"><img src="https://rawgit.com/vedantvohra1/bot-bolt/master/tony.jpg" alt="User" style="width:5%;height:5%;" class="right" style="width:100%;"><p>' + text + '</p><span class="time-left">' + time + '</span></div>');
+        $('#messages').append('<div class="container darker"><img src="' + imgs.float_icon + '" alt="User" style="width:5%;height:5%;" class="right" style="width:100%;"><p>' + text + '</p><span class="time-left">' + time + '</span></div>');
         $('.modal-body').html("Please wait...");
         $.ajax({
             type: "GET",
             url: "http://localhost:3000/query?q=" + encodeURIComponent(text),
+            crossDomain: true, //set as a cross domain requests 
+            withCredentials: false,
             success: function(response) {
                 console.log(response);
 
                 if (response.url) {
-                    var url = base_url + response.url;
+                    var url = `${base_url}${response.url}?bot=true`;
                     console.log(url);
                     localStorage.setItem("init", response.init);
                     console.log(response.init);
                     localStorage.setItem("entities", JSON.stringify(response.entities));
                     console.log(response.entities);
                     $('.modal-body').empty();
-                    //?q=apply_leave
-                    $('.modal-body').append('<iframe id="myframe" frameBorder="0" src = "' + url + '?q=apply_leave" width = "100%" height = "900px">Sorry your browser does not support inline frames.</iframe>');
+                    $('.modal-body').append('<iframe id="myframe" frameBorder="0" src = "' + url + '" width = "100%" height = "400px">Sorry your browser does not support inline frames.</iframe>');
                     console.log("assiging init");
-                    var init = localStorage.getItem("init");
-                    console.log(init);
-                    var entities = localStorage.getItem("entities");
-                    var jq = '<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>';
-                    var scriptTag = '<script type="text/javascript" src="' + cdn + init + '.js#L11">console.log(" init is actually called");init(' + entities + '); </script>';
-                    var test = '<script>console.log(document.getElementsByTagName("body").innerHTML);</script>';
-                    //please don't remove the following content please
-                    //uncomment any of block one or two to append the script to the page in iframe 
-                    //start of block one
-
-
-                    $("#myframe").contents().find("head").append(jq);
-                    $("#myframe").contents().find("head").append(scriptTag);
-                    sleep(4);
-                    $("#myframe").contents().find("head").append(test);
-                    console.log($("#myframe").contents());
-                    //end of block one
-
-                    //start of block two
-                    // var x = document.getElementById("myframe");
-                    // var y = (x.contentWindow || x.contentDocument);
-                    // if (y.document) y = y.document;
-                    // y.body.style.backgroundColor = "red";
-                    // y.head.append(test);
-                    // y.head.append('<script src="' + cdn + init + '.js"></script>');
-                    // y.head.append('<script src="' + cdn + init + '.js"></script>');
-                    //end of block two
                 } else {
                     $('.modal-body').empty();
                     botsay("Hmm... I'm sorry, could you say that again?");
@@ -121,7 +119,7 @@ function scrollToBottom() {
 
 function botpop() {
     voiceInit();
-    localStorage.clear();
+    //localStorage.clear();
     console.log("bot is init");
     $('body').append('<div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><div id="messages"></div></div><div class="modal-body"></div><div id="reqblock" class="modal-footer"></div></div></div></div></div>');
     $("#messages").empty();
@@ -129,6 +127,7 @@ function botpop() {
     $("#reqblock").empty();
     $(".modal-body").empty();
     $('#reqblock').append('<input type="text" id="message">');
+
     $('#reqblock').append('<button id="callbot" type="button" class="btn btn-default" >Record</button>');
     $('#reqblock').append('<button id="textsubmit" type="button" class="btn btn-default" >submit</button>');
     $("#callbot").click(function() {
@@ -145,19 +144,10 @@ function botpop() {
 
 var voiceInit = () => {
     flags.listening = false;
-
-    try
-
-    {
-
+    try {
         recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition || SpeechRecognition || webkitSpeechRecognition)();
-
-    } catch (e)
-
-    {
-
+    } catch (e) {
         console.log("Your browser does not support speech recognition. Please use Chrome.", e);
-
     }
 }
 
@@ -167,98 +157,61 @@ function botsay(message) {
         m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
     var time = h + ':' + m;
     console.log(time);
-    $('#messages').append('<div class="container lighter"><img src="https://rawgit.com/vedantvohra1/bot-bolt/master/jarvis.png" alt="Darwin" style="width:5%;height:5%;"><p>' + message + '</p><span class="time-right">' + time + '</span></div>');
+    $('#messages').append('<div class="container lighter"><img src="' + imgs.float_icon + '" alt="Darwin" style="width:5%;height:5%;"><p>' + message + '</p><span class="time-right">' + time + '</span></div>');
 }
 
 var voiceInit = () => {
     flags.listening = false;
 
-    try
-
-    {
-
+    try {
         recognition = new(window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition || SpeechRecognition || webkitSpeechRecognition)();
-
-    } catch (e)
-
-    {
-
+    } catch (e) {
         console.log("Your browser does not support speech recognition. Please use Chrome.", e);
-
     }
 }
 
-var getTextFromEvent = function(e)
-
-{
-
+var getTextFromEvent = function(e) {
     return event.results[0][0].transcript;
-
 };
 
-var trigger_speech_recognition = function()
-
-{
-
-
+var trigger_speech_recognition = function() {
     recognition.continuous = false;
-
     recognition.interimResults = true;
-
     recognition.lang = "en-US";
-
     recognition.maxAlternatives = 1;
-
     var t = $(output_class).val(); // existing text
-
     recognition.onresult = function(event) {
-
         var r = getTextFromEvent(event); // new word
-
         $(output_class).val(t + " " + r) //reads existing content and add new identified word with a space
-
     };
 
     recognition.onstart = function() {
-
         console.log("listening");
         flags.listening = true;
         $('#callbot').html('Recording...')
     }
 
-
-
     recognition.onend = function() {
-
         flags.listening = false;
         $('#callbot').html('Record');
-
         console.log("onend")
         if (flags.manual_intervention) {
             message();
             return;
         }
-
         trigger_speech_recognition();
-
     }
-
-
 
     recognition.onerror = function(event) {
 
         if (event.error == 'no-speech') {
-
             console.log('no-speech');
-
             stop_speech_recognition();
-
         };
 
     }
 
     recognition.start();
-
 
     recognition.onaudioend = function(event) {
         console.log("audioend");
@@ -269,11 +222,39 @@ var trigger_speech_recognition = function()
 };
 
 var stop_speech_recognition = function(manual = false) {
-
     recognition.stop();
     flags.listening = false;
     $('#callbot').html('Record');
 };
+
+
+
+
+
+
+function sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+
+    while (currentTime + miliseconds >= new Date().getTime()) {}
+}
+
+
+function scrollToBottom() {
+    $('html, body').animate({
+        scrollTop: $(document).height()
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
