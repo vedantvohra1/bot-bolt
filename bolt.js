3fc17d7755e33acff93ce1a5bf91a5b9721aafe7
nvk681 <nvk.681@gmail.com>
2018-03-07 11:13:20 +0530

    hide and unhide iframe

--- bolt.js
+++ bolt.js
@@ -48,6 +48,16 @@ function sleep(miliseconds) {
     while (currentTime + miliseconds >= new Date().getTime()) {}
 }
 
+function Editiframe() {
+    if (document.getElementById('myframe').style.display == 'block') {
+        document.getElementById('myframe').style.display = 'none';
+        document.getElementById('mapiframe').innerText = '>>';
+    } else {
+        document.getElementById('myframe').style.display = 'block';
+        document.getElementById('mapiframe').innerText = '^';
+    }
+}
+
 function message() {
     $('.modal-body').empty();
     var text = $('#message').val();
@@ -76,8 +86,11 @@ function message() {
                     localStorage.setItem("entities", JSON.stringify(response.entities));
                     console.log(response.entities);
                     $('.modal-body').empty();
-                    $('.modal-body').append('<iframe id="myframe" frameBorder="0" src = "' + url + '" width = "100%" height = "400px">Sorry your browser does not support inline frames.</iframe>');
+                    $('.modal-body').append('<button type="button"  id="mapiframe" >^</button><iframe id="myframe" frameBorder="0" src = "' + url + '" width = "100%" height = "400px">Sorry your browser does not support inline frames.</iframe>');
                     console.log("assiging init");
+                    document.getElementById('myframe').style.display = 'block';
+                    document.getElementById('mapiframe').onclick = function() { Editiframe(); };
+
 
                     var resp = JSON.parse(localStorage.getItem("resp"))
                     if (resp) {
